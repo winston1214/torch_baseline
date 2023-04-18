7574fb0532e2a1de8cad26088b9ac22fd6969418
unknown <winston1214@naver.com>
2023-04-18 17:06:49 +0900

    multi-label classification

--- multi_label_classification/test.py
+++ multi_label_classification/test.py
@@ -8,7 +8,14 @@ import argparse
 import torchvision.transforms as T
 import torch.utils.data as D
 from tqdm import tqdm
+from sklearn.metrics import f1_score
 
+def write_csv(pred_list):
+    test_name = sorted(os.listdir('/data/dmc/3_dota/clean_test'))
+    column_name = ['bald','blood','blush','dandruff','dyed','normal','powder','tattoo']
+    df = pd.DataFrame(index=test_name, data=pred_list,columns = column_name)
+    return df
+    
 def test(opt):
     device = 'cuda:0'
     pretrained = timm.create_model('resnet50')
@@ -16,36 +23,48 @@ def test(opt):
     model.load_state_dict(torch.load(opt.ckpt))
     model.to(device)
     processing = Proces_dataset(opt.path, 'instances_default.json')
-    test_img,test_label = processing.train_val_test('data/dmc/3_dota','test')
+    test_img,test_label = processing.train_val_test('/data/dmc/3_dota','test')
     test_transforms = T.Compose([
         T.ToTensor(),
         T.Normalize(
             [0.485, 0.456, 0.406],
             [0.229, 0.224, 0.225]
             ),
+#         AddGaussianNoise(0., 1.),
+#         T.RandomCrop(480)
     ])
 
     test_dataset = ScalpDataset(opt.path+'images/', test_img,test_label,transform = test_transforms)
     test_dataloader = D.DataLoader(test_dataset, batch_size = 16, shuffle = False, drop_last=False)
     prediction_list = np.array([])
     model.eval()
+    acc_list = []
     with torch.no_grad():
         for images, labels in tqdm(test_dataloader):
             images = images.type(torch.FloatTensor).to(device)
             labels = labels.type(torch.FloatTensor).to(device)
 
             probs = model(images)
-            
+            probs = torch.sigmoid(probs)
             probs = probs.cpu().detach().numpy()
+            labels = labels.cpu().detach().numpy()
+            prediction_list = np.append(prediction_list,probs).reshape(-1,8)
             preds = probs > 0.5
-            prediction_list = np.append(prediction_list,preds.astype(np.int)).reshape(-1,8)
+#             prediction_list = np.append(prediction_list,preds.astype(np.int)).reshape(-1,8)
+            batch_acc = f1_score(preds,labels,average='macro')
+            acc_list.append(batch_acc)
+    print(np.mean(acc_list))
+    
     return prediction_list
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path',type=str,default='data/dmc/2_coco/')
+    parser.add_argument('--path',type=str,default='/data/dmc/2_coco/')
     parser.add_argument('--ckpt',type=str)
+    parser.add_argument('--result_csv',type=str,default = 'results.csv')
     opt = parser.parse_args()
     pred = test(opt)
-    
-    print(sum(pred))
+    df = write_csv(pred)
+    df.to_csv(opt.result_csv,index=False)
+    print('Done')
+
--- multi_label_classification/train.py
+++ multi_label_classification/train.py
@@ -2,14 +2,11 @@ from dataset import ScalpDataset,Proces_dataset
 from model import StyleModel
 
 import os
-import cv2
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings('ignore')
 from tqdm import tqdm
-
 
 import torch
 import torch.nn as nn
@@ -19,8 +16,6 @@ import torchvision.transforms as T
 import torchvision.models as models
 
 import argparse
-
-
 import timm
 import random
 import torch.backends.cudnn as cudnn
@@ -33,28 +28,30 @@ cudnn.benchmark = False
 cudnn.deterministic = True
 random.seed(0)
 device = 'cuda:0'
-import random
-import torch.backends.cudnn as cudnn
-import numpy as np
-torch.manual_seed(0)
-torch.cuda.manual_seed(0)
-torch.cuda.manual_seed_all(0)
-np.random.seed(0)
-cudnn.benchmark = False
-cudnn.deterministic = True
-random.seed(0)
-device = 'cuda:0'
+
+class AddGaussianNoise(object):
+    def __init__(self, mean=0., std=1.):
+        self.std = std
+        self.mean = mean
+        
+    def __call__(self, tensor):
+        return tensor + torch.randn(tensor.size()) * self.std + self.mean
+    
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+
 def train(opt):
     os.makedirs(opt.save_dir, exist_ok=True)
     processing = Proces_dataset(opt.path, 'instances_default.json')
-    train_img,train_label = processing.train_val_test('data/dmc/3_dota','train')
-    val_img,val_label = processing.train_val_test('data/dmc/3_dota','val')
+    train_img,train_label = processing.train_val_test('/data/dmc/3_dota','train')
+    val_img,val_label = processing.train_val_test('/data/dmc/3_dota','val')
     transformer = T.Compose([
         T.ToTensor(),
         T.Normalize(
             [0.485, 0.456, 0.406],
-            [0.229, 0.224, 0.225]
-            )
+            [0.229, 0.224, 0.225]),
+#         AddGaussianNoise(0., 1.),
+#         T.RandomCrop(480)
         ])
     train_dataset = ScalpDataset(opt.path+'images/', train_img,train_label,transform = transformer)
     val_dataset = ScalpDataset(opt.path+'images/', val_img,val_label,transform = transformer)
@@ -72,13 +69,14 @@ def train(opt):
     pretrained = timm.create_model('resnet50',pretrained=True)
     model = StyleModel(pretrained)
     model.to(device)
-    optimizer = torch.optim.SGD(model.parameters(),lr = 1e-3)
-    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer,T_max=0.1,eta_min=0.0001)
+    optimizer = torch.optim.Adam(model.parameters(),lr = 1e-3)
+    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer,T_max=50,eta_min=1e-4)
     criterion = torch.nn.BCEWithLogitsLoss(pos_weight = pos_weight)
+#     criterion = FocalLoss2d(weight = pos_weight)
     criterion.to(device)
     total_step = len(train_dataloader)
     best_val_acc = 0
-    EPOCH = 30
+    EPOCH = opt.epoch
     train_loss_ls = []
     for epoch in range(EPOCH):
         train_acc_list = []
@@ -92,17 +90,19 @@ def train(opt):
             optimizer.zero_grad()
 
             probs= model(images)
-            
+
             loss = criterion(probs, labels)
             loss.backward()
             optimizer.step()
             
             running_loss += loss.item()
             
+#             probs = torch.sigmoid(probs)
             probs  = probs.cpu().detach().numpy()
             labels = labels.cpu().detach().numpy()
             preds = probs > 0.5
-            batch_acc = (labels == preds).mean()
+#             batch_acc = (labels == preds).mean()
+            batch_acc = f1_score(labels,preds,average='macro')
             train_acc_list.append(batch_acc)
         
         train_acc = np.mean(train_acc_list)
@@ -119,12 +119,12 @@ def train(opt):
 
                 probs = model(images)
                 # valid_loss = criterion(probs, labels)
-
+#                 probs = torch.sigmoid(probs)
                 probs  = probs.cpu().detach().numpy()
                 labels = labels.cpu().detach().numpy()
                 
                 preds = probs > 0.5
-                batch_acc = (labels == preds).mean()
+                batch_acc = f1_score(labels,preds,average='macro')
                 valid_acc_list.append(batch_acc)
                 
             val_acc = np.mean(valid_acc_list)
@@ -135,11 +135,12 @@ def train(opt):
             torch.save(model.state_dict(), f'{opt.save_dir}best_cls_{epoch}.pth')
         torch.save(model.state_dict(), f'{opt.save_dir}cls_{epoch}.pth')
     plt.plot(train_loss_ls)
-
+    plt.savefig('/workspace/train_loss.png')
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path',type=str,default='data/dmc/2_coco/')
+    parser.add_argument('--epoch',type=int,default=30)
+    parser.add_argument('--path',type=str,default='/data/dmc/2_coco/')
     parser.add_argument('--batch',type=int,default=16)
-    parser.add_argument('--save_dir',type=str,default = 'data/dmc/cls_ckpt/')
+    parser.add_argument('--save_dir',type=str,default = '/data/dmc/cls_ckpt/')
     opt = parser.parse_args()
     train(opt)
